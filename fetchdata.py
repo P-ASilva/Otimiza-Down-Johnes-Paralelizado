e2388a795dfa0028f13f794dcf21805d93a67055
P-ASilva <pedroas5.al.insper.edu>
2025-05-06 11:05:21 -0300

    feat: first version for data loader

---
+++ fetchdata.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import pandas as pd
+
+tickers = [
+    "AAPL", "AMGN", "AXP", "BA", "CAT", "CSCO", "CVX", "DIS", "DOW", "GS",
+    "HD", "HON", "IBM", "INTC", "JNJ", "JPM", "KO", "MCD", "MMM", "MRK",
+    "MSFT", "NKE", "PG", "CRM", "TRV", "UNH", "V", "VZ", "WBA", "WMT"
+]
+
+start_date = "2024-07-01"
+end_date = "2024-12-31"
+
+data = yf.download(tickers, start=start_date, end=end_date)["Close"]
+
+data.to_csv("./data/dow_jones_2024_S2_closing_prices.csv")
+
+print("Dados baixados com sucesso.")
+print(data.head())
